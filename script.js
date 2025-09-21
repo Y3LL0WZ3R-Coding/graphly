8519f83e2998cc1b61de181db2163d30f9a04740
Y3LL0WZ3R-Coding <nikolajplis24@gmail.com>
2025-09-21 16:57:41 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function sin(x) {
+ return Math.sin(x);
+}
+function cos(x) {
+ return Math.cos(x);
+}
+function tan(x) {
+ return Math.tan(x);
+}
+function cot(x) {
+ return 1/tan(x);
+}
+function sec(x) {
+ return 1/cos(x);
+}
+function csc {
+ return 1/sin(x);
+}
